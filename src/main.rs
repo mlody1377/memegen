6e89e25a1bbb3ccadf2fed8c2a1cd7f7ad64f409
Karol <kkarolek0000@gmail.com>
2025-05-16 08:17:08 +0200

    rusttt

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
+use std::io::{self, Write};
+use reqwest::blocking::Client;
+
 
 fn main() {
     println!("Please enter the text you want to add to your meme:");
     let mut input = String::new();
-
+    
+    let response = reqwest::blocking::get("url");
     
 }
